567bb29b0942d4dfa327800122436569d0a8850b
MohammadJozaa7 <m.jozaa@yahoo.com>
2019-07-09 16:33:05 +0300

    upload till questions 8

--- recursion.js
+++ recursion.js
@@ -1,30 +1,25 @@
+// all the exercises should use recursion:
+
 /*
 1
-Create a function called sum 
-that takes a single parameter n, 
+Create a function called sum
+that takes a single parameter n,
 and return the sum of all integers up to n starting from 0
 
-Ex: sum(2); => 2 + 1 + 0 => 3 
+Ex: sum(2); => 2 + 1 + 0 => 3
 Ex: sum(5); => 5 + 4 + 3 + 2 + 1 + 0 => 15
-
 */
-function sum(n) {
-  // your code here 
-}
+
 
 /*
 2
-Create a function called factorial 
-that takes a single parameter n, 
+Create a function called factorial
+that takes a single parameter n,
 and return the product of all integers up to n starting from 1
 
-Ex: factorial(2); => 2 * 1 => 6 
-Ex: factorial(4); => 4 * 3 * 2 * 1 => 24 
-
+Ex: factorial(2); => 2 * 1 => 6
+Ex: factorial(4); => 4 * 3 * 2 * 1 => 24
 */
-function factorial(n) {
-  // your code here 
-}
 
 
 /*
@@ -32,6 +27,8 @@ function factorial(n) {
 Create a function called repeatStr
 that takes 2 parameters string and number,
 and return the string number time with space
+Important: the stop condition should be [somthing=== 0]
+** Think how we can return nothing ???
 
 Ex: repeatStr("to",2); => "to to"
 Ex: repeatStr("to",4); => "to to to to"
@@ -49,41 +46,56 @@ Ex: fibon(2); => 2
 Ex: fibon(1); => 1
 Ex: fibon(0); => 1
 
-fib(n): 1 1 2 3 5 8 13 21
-        | | | | | |  |  |
-n:      0 1 2 3 4 5  6  7
+fibon(n): 1 1 2 3 5 8 13 21
+          | | | | | |  |  |
+n:        0 1 2 3 4 5  6  7
 */
 
 
 /*
+5
+Create a function called sum2
+that take two parameter
+and will return the sumation from the first number to the second number
 
-
-
-
+Ex: sum2(4, 5); => 4 + 5 => 9
+Ex: sum2(3, 6); => 3 + 4 + 5 + 6 => 18
 */
 
 
 /*
+6
+Create a function called repeatStr2
+that takes 2 string parameters,
+and return the first string number time
+(of the lenght of the secend string) with space betweeen it
+Important: the stop condition should be [somthing=== 0]
 
-
-
-
+Ex: repeatStr2("ro","cc"); => "ro ro"
+Ex: repeatStr2("ro","fff"); => "ro ro ro"
 */
 
 
 /*
+7
+Create a function called multiOf
+that takes 3 parameters,
+and return the first number mutiple by
+the second one ^ the third one
 
-
-
-
+Ex: multiOf(4,3,10); => 4000
+Ex: multiOf(6,2,4); => 96
 */
 
 
 /*
+8
+Create a function called muti2
+that take two parameter
+and will return the multiplication from the first number to the second number
 
-
-
-
+Ex: muti2(4, 5); => 4 * 5 => 20
+Ex: muti2(3, 6); => 3 * 4 * 5 * 6 => 360
 */
 
 
